c520a0a7b872772bf2297785ff07110cad3303a2
rijul <96693553+kansalrijul@users.noreply.github.com>
2022-04-18 00:48:02 +0530

    Create rating917.cpp

---
+++ rating917.cpp
@@ -0,0 +1,17 @@
+https://www.codechef.com/problems/UWCOI21A
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a;
+        cin>>a;
+        cout<<1<<" "<<a<<endl;
+    }
+    return 0;
+}
