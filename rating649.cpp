4b85e5a82af8de8588fd78be492fd90747978abc
rijul <96693553+kansalrijul@users.noreply.github.com>
2022-04-18 00:22:22 +0530

    Create rating649.cpp

---
+++ rating649.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int a;
+        cin>>a;
+        if(a%3==0)
+        {
+            cout<<"NORMAL"<<endl;
+        }
+        else if(a%3==1)
+        {
+            cout<<"HUGE"<<endl;
+        }
+        else
+        {
+            cout<<"SMALL"<<endl;
+        }
+    }
+    
+    return 0;
+}
