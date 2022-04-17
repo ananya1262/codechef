dc74502f5fe7953ba956657515fb370ab5881698
rijul <96693553+kansalrijul@users.noreply.github.com>
2022-04-18 00:43:08 +0530

    Create rating889.py

---
+++ rating889.py
@@ -0,0 +1,9 @@
+https://www.codechef.com/problems/CHFPROFIT
+  for  i in range(int(input())):
+    a,b,c=input().split()
+    a=int(a)
+    b=int(b)
+    c=int(c)
+    aa=a*b
+    bb=a*c
+    print(bb-aa)# cook your dish here
