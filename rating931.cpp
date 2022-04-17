dda39320c900cb959aff2dfa3ad1c50d03699d2b
rijul <96693553+kansalrijul@users.noreply.github.com>
2022-04-17 22:57:12 +0530

    Create rating931.cpp

---
+++ rating931.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n,k;
+        cin>>n>>k;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin>>arr[i];
+        }
+        
+        int sum=0;
+        for (int i = 0; i < n; i++)
+        {
+            sum=sum+arr[i];
+        }
+        
+        for (int i = 0; i < n; i++)
+        {
+            if(arr[i]>k)
+             arr[i]=k;
+        }
+         
+        int sum1=0;
+        for (int i = 0; i < n; i++)
+        {
+            sum1=sum1+arr[i];
+        } 
+
+        cout<<sum-sum1<<endl;
+
+        
+
+        
+    }
+    
+    return 0;
+}
