0e211c1e159f6e44888a964763a12d327a3e6cc1
rijul <96693553+kansalrijul@users.noreply.github.com>
2022-04-18 01:02:27 +0530

    Create rating998.cpp

---
+++ rating998.cpp
@@ -0,0 +1,20 @@
+https://www.codechef.com/problems/BLITZ3_2
+include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int a,b,cc;
+        cin>>a>>b>>cc;
+
+        cout<<2*(180+a)-(b+cc)<<endl;
+
+    }
+    
+    return 0;
+}
