d33e61167b8b881a69cf76586d4329fdd36fb2d7
rijul <96693553+kansalrijul@users.noreply.github.com>
2022-04-17 22:25:28 +0530

    Create rating804.cpp

---
+++ rating804.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s1,s2;
+        string s3;
+        cin>>s1>>s2;
+        for (int i = 0; i < s1.length(); i++)
+        {
+            if(s1.at(i)==s2.at(i))
+            s1[i]='G';
+            else
+            s1.at(i)='B';
+        }
+        cout<<s1<<endl;
+        
+    }
+    return 0;
+}
