22dd29413e951e4ea77cdcbd98e4e36632bfbb23
rijul <96693553+kansalrijul@users.noreply.github.com>
2022-04-18 00:39:51 +0530

    Create rating855.py

---
+++ rating855.py
@@ -0,0 +1,18 @@
+https://www.codechef.com/problems/CHEFTRANS
+  try:
+ list1=[]
+ for i in range(int(input())):
+    a,b,c=input().split()
+    a=int(a)
+    b=int(b)
+    c=int(c)
+    if a+b>c:
+        list1.append("TRAIN")
+    elif a+b<c:
+        list1.append("PLANEBUS")
+    else:
+        list1.append("EQUAL")
+ for i in list1:
+  print(i) 
+except:
+ pass       
