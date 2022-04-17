7099e5ccbe06e36bdbe280b42c824c1c4036e1aa
rijul <96693553+kansalrijul@users.noreply.github.com>
2022-04-17 23:12:49 +0530

    Create rating957.py

---
+++ rating957.py
@@ -0,0 +1,10 @@
+list1=[]
+
+for i in range(int(input())):
+    a=list(map(int,input().split()))
+    list1.append(a)
+for i in list1:   
+  if i[0]==i[2] or i[1]==i[3]:
+    print("YES")
+  else:
+    print("NO")
