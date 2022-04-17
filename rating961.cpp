87e9be678443334251127b528e40946d9ea1686b
rijul <96693553+kansalrijul@users.noreply.github.com>
2022-04-17 23:14:50 +0530

    Create rating961.cpp

---
+++ rating961.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+
+
+    while(t--){
+        int d;
+        int n;
+
+        cin>>d>>n;
+
+        while(d--){
+            n=n*(n+1)/2;
+
+        }
+        cout<<n<<endl;
+    }
+}
