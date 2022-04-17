73436faee121ef34f1fc59098ebe595825c8c7e2
rijul <96693553+kansalrijul@users.noreply.github.com>
2022-04-18 00:55:40 +0530

    Create rating964.cpp

---
+++ rating964.cpp
@@ -0,0 +1,17 @@
+https://www.codechef.com/problems/CYBV
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b;
+        cin>>a>>b;
+        cout<<b/a<<endl;
+    }
+    return 0;
+}
